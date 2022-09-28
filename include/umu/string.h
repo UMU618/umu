3f8998078411538517233bf9e79721010d56f8a5
UMU618 <umu618@hotmail.com>
2022-09-29 01:27:48 +0800

    * split

--- include/umu/string.h
+++ include/umu/string.h
@@ -13,8 +13,19 @@ template <size_t N>
 #if _HAS_CXX20
 constexpr
 #endif
-std::string ArrayJoin(
-    const std::array<std::string_view, N>& array) {
+    std::string
+    ArrayJoin(const std::array<std::string_view, N>& array) {
+  auto buffer = std::string("{").append(array.at(0));
+  for (size_t i = 1; i < N; ++i) {
+    buffer.append(", ").append(array.at(i));
+  }
+  return buffer.append("}");
+}
+#pragma endregion
+
+#pragma region "Join"
+template <size_t N>
+constexpr std::string ArrayJoin(const std::array<std::string_view, N>& array) {
   auto buffer = std::string("{").append(array.at(0));
   for (size_t i = 1; i < N; ++i) {
     buffer.append(", ").append(array.at(i));
@@ -26,42 +37,66 @@ std::string ArrayJoin(
 #pragma region "Split"
 // source_string 末尾如果是 separator，其后的空白不被加入
 // container，只有中间的空白会被加入 container
-template <class StringType>
-inline typename StringType::size_type Split(std::vector<StringType>* container,
-                                            const StringType& source_string,
-                                            const StringType& separator) {
-  if (NULL != container) {
+template <class StringType, class StringOrCharType>
+inline typename StringType::size_type split(
+    std::vector<StringType>* container,
+    const StringType& source_string,
+    const StringOrCharType separator,
+    const typename StringType::size_type separator_size) {
+  using size_type = typename StringType::size_type;
+
+  if (nullptr != container) {
     container->clear();
   }
-
-  size_t size = 0;
-  typename StringType::size_type legth = source_string.length();
-  typename StringType::size_type start = 0;
-  typename StringType::size_type end;
+  const size_type source_size = source_string.size();
+  size_type size = 0;
+  size_type start = 0;
+  size_type end;
 
   do {
     end = source_string.find(separator, start);
     StringType e(source_string.substr(start, end - start));
-    if (NULL != container) {
+    if (nullptr != container) {
       container->push_back(e);
     }
-    start = end + separator.length();
+    start = end + separator_size;
     ++size;
-  } while (start < legth && StringType::npos != end);
+  } while (start < source_size && StringType::npos != end);
 
   return size;
 }
-template <class StringType, typename CharType>
+
+template <class StringType>
 inline typename StringType::size_type Split(std::vector<StringType>* container,
                                             const StringType& source_string,
-                                            const CharType* separator) {
+                                            const StringType& separator) {
+  const auto separator_size = static_cast<StringType>(separator).size();
+  return split(container, source_string, separator, separator_size);
+}
+
+template <class StringType>
+inline typename StringType::size_type Split(
+    std::vector<StringType>* container,
+    const StringType& source_string,
+    const typename StringType::value_type separator) {
+  const typename StringType::size_type separator_size = 1;
+  return split(container, source_string, separator, separator_size);
+}
+
+template <class StringType>
+inline typename StringType::size_type Split(
+    std::vector<StringType>* container,
+    const StringType& source_string,
+    const typename StringType::value_type* separator) {
   return Split(container, source_string, StringType(separator));
 }
-template <class StringType, typename CharType>
-inline typename StringType::size_type Split(std::vector<StringType>* container,
-                                            const CharType* source_string,
-                                            const CharType* separator) {
-  return Split(container, StringType(source_string), StringType(separator));
+
+template <class StringType, class StringOrCharType>
+inline typename StringType::size_type Split(
+    std::vector<StringType>* container,
+    const typename StringType::value_type* source_string,
+    const typename StringOrCharType separator) {
+  return Split(container, StringType(source_string), separator);
 }
 #pragma endregion
 
@@ -71,14 +106,15 @@ inline typename StringType::size_type SplitAnyOf(
     std::vector<StringType>* container,
     const StringType& source_string,
     const StringType& token) {
+  using size_type = typename StringType::size_type;
+
   if (nullptr != container) {
     container->clear();
   }
-
-  size_t size = 0;
-  typename StringType::size_type legth = source_string.length();
-  typename StringType::size_type start = 0;
-  typename StringType::size_type end;
+  const size_type source_size = source_string.size();
+  size_type size = 0;
+  size_type start = 0;
+  size_type end;
 
   do {
     end = source_string.find_first_of(token, start);
@@ -88,22 +124,24 @@ inline typename StringType::size_type SplitAnyOf(
     }
     start = end + 1;
     ++size;
-  } while (start < legth && StringType::npos != end);
+  } while (start < source_size && StringType::npos != end);
 
   return size;
 }
-template <class StringType, typename CharType>
+
+template <class StringType>
 inline typename StringType::size_type SplitAnyOf(
     std::vector<StringType>* container,
     const StringType& source_string,
-    const CharType token) {
+    const typename StringType::value_type* token) {
   return SplitAnyOf(container, source_string, StringType(token));
 }
-template <class StringType, typename CharType>
+
+template <class StringType>
 inline typename StringType::size_type SplitAnyOf(
     std::vector<StringType>* container,
-    const CharType source_string,
-    const CharType token) {
+    const typename StringType::value_type* source_string,
+    const typename StringType::value_type* token) {
   return SplitAnyOf(container, StringType(source_string), StringType(token));
 }
 #pragma endregion
@@ -126,23 +164,28 @@ inline typename StringType::size_type Replace(StringType& source_string,
   }
   return replace_times;
 }
-template <class StringType, typename CharType>
-inline typename StringType::size_type Replace(StringType& source_string,
-                                              const StringType& find,
-                                              const CharType* replace_with) {
+
+template <class StringType>
+inline typename StringType::size_type Replace(
+    StringType& source_string,
+    const StringType& find,
+    const typename StringType::value_type* replace_with) {
   return Replace(source_string, find, StringType(replace_with));
 }
-template <class StringType, typename CharType>
-inline typename StringType::size_type Replace(StringType& source_string,
-                                              const CharType* find,
-                                              const CharType* replace_with) {
+
+template <class StringType>
+inline typename StringType::size_type Replace(
+    StringType& source_string,
+    const typename StringType::value_type* find,
+    const typename StringType::value_type* replace_with) {
   return Replace(source_string, StringType(find), StringType(replace_with));
 }
 
-template <class StringType, typename CharType>
-inline typename StringType::size_type Replace(StringType& source_string,
-                                              const CharType find,
-                                              const CharType replace_with) {
+template <class StringType>
+inline typename StringType::size_type Replace(
+    StringType& source_string,
+    const typename StringType::value_type find,
+    const typename StringType::value_type replace_with) {
   typename StringType::size_type replace_times = 0;
   for (auto it = source_string.begin(); it != source_string.end(); ++it) {
     if (*it == find) {
