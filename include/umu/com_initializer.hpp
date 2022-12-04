cb6e0dce337288391f2d4ef7334ae206be13281b
UMU <umu618@hotmail.com>
2022-12-05 00:17:58 +0800

    + Uninitialize for ComInitializer

--- include/umu/com_initializer.hpp
+++ include/umu/com_initializer.hpp
@@ -4,11 +4,7 @@ namespace umu {
 class ComInitializer {
  public:
   ComInitializer() : hr_(CO_E_NOTINITIALIZED) {}
-  ~ComInitializer() {
-    if (SUCCEEDED(hr_)) {
-      ::CoUninitialize();
-    }
-  }
+  ~ComInitializer() { Uninitialize(); }
 
   HRESULT Initialize(DWORD type = COINIT_APARTMENTTHREADED) {
     // https://docs.microsoft.com/en-us/windows/win32/learnwin32/initializing-the-com-library
@@ -17,6 +13,14 @@ class ComInitializer {
     // Linking and Embedding (OLE) 1.0, an obsolete technology.
     hr_ = CoInitializeEx(nullptr, type | COINIT_DISABLE_OLE1DDE);
     return hr_;
+  }
+
+  // Usually not necessary to call
+  void Uninitialize() noexcept {
+    if (SUCCEEDED(hr_)) {
+      hr_ = CO_E_NOTINITIALIZED;
+      ::CoUninitialize();
+    }
   }
 
  private:
